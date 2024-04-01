d5a1374ecea5b2ac7e3765f07068037cff83378a
Maik2805 <mscardenas77@misena.edu.co>
2024-04-01 17:29:18 -0500

    implementación superficie del mundo

--- src/lights/Lights.jsx
+++ src/lights/Lights.jsx
@@ -5,42 +5,52 @@ import { Color } from "three";
 const Lights = () => {
   const optionsSpotLight = useMemo(() => {
     return {
-      intensitySL:{ value:1000, min: 0, max:1000, step:1},
-      colorSL:{value:'#bfff00'},
+      intensitySL: { value: 1000, min: 0, max: 1000, step: 1 },
+      colorSL: { value: '#bfff00' },
     }
   }
-  , []);
+    , []);
 
-  const {intensitySL, colorSL} = useControls('SpotLight', optionsSpotLight);
+  // const { intensitySL, colorSL } = useControls('SpotLight', optionsSpotLight);
 
   return (
     <>
-        <ambientLight  intensity={1} />
-        
-        {/* esta luz es */}
-        <directionalLight position={[2, 10,0]}
-        castShadow={true}
-        color={new Color('#8f00ff')}
-        intensity={1} 
-        />
+      <ambientLight
+        color={new Color("#FFFFFF")}
+        intensity={0.5}
+      />
 
-        {/* esta luz es una luz puntual pero es costosa a nivel de rendimiento */}
-        {/* <pointLight position={[0, 10, 40]} 
+      {/* esta luz es */}
+      {/* <directionalLight
+        castShadow={true}
+        position={[2, 10, 0]}
+        color={new Color("#FFF700")}
+        intensity={2}
+        shadow-mapSize={[2048, 2048]}
+        shadow-camera-far={50}
+        shadow-camera-left={-10}
+        shadow-camera-right={10}
+        shadow-camera-top={10}
+        shadow-camera-bottom={-10}
+      /> */}
+
+      {/* esta luz es una luz puntual pero es costosa a nivel de rendimiento */}
+      {/* <pointLight position={[0, 10, 40]} 
         color={new Color('#8f00ff')}
         intensity={1000} /> */}
 
-        <spotLight position={[0, 5, 40]}
-        angle={Math.PI/3}
+      {/* <spotLight position={[0, 5, 40]}
+        angle={Math.PI / 3}
         color={colorSL}
         intensity={intensitySL}
-        ></spotLight>
+      ></spotLight>
 
-        <hemisphereLight 
+      <hemisphereLight
         position={[2, 10, -2]}
         intensity={3}
         color={"red"}
         skyColor={"blue"}
-        />
+      /> */}
 
     </>
   );
--- src/world/World.jsx
+++ src/world/World.jsx
@@ -1,44 +1,50 @@
-import {useGLTF, useTexture} from "@react-three/drei"
-import { Color, RepeatWrapping,MeshBasicMaterial } from 'three';
+import { useGLTF, useTexture } from "@react-three/drei"
+import { Color, RepeatWrapping, MeshBasicMaterial } from 'three';
 
-export default function World(props){
-    // const {nodes, materials} =useGLTF('/assets/models/world/squisgame.glb');
-    const {nodes, materials} =useGLTF('/assets/models/world/nave_espacial.glb');
-    console.log("hola")
-    console.log(nodes)
-    const PATH = '/assets/textures/floor/';
-    // const propsTexture =  useTexture({
-    //     map : PATH + 'gray_rocks_diff_1k.jpg',
-    //     displacementMap: PATH + 'gray_rocks_disp_1k.png',
-    //     normalMap: PATH + 'gray_rocks_nor_dx_1k.jpg',
-    //     roughnessMap: PATH + 'gray_rocks_rough_1k.jpg',
-    // }
-    // ) 
-    // console.log(propsTexture)
-    
-    // propsTexture.map.repeat.set(4,64);
-    // propsTexture.map.wrapS = propsTexture.map.wrapT = RepeatWrapping;
+export default function World(props) {
+  // const {nodes, materials} =useGLTF('/assets/models/world/squisgame.glb');
+  const { nodes, materials } = useGLTF('/assets/models/world/nave_espacial.glb');
+  const { nodes: nodesSG, materials: materialsSG } = useGLTF("/assets/models/world/SquidGamesMaik.glb");
+  const PATH = "/assets/textures/moon_floor/";
+  const propsTexture = useTexture({
+    map: PATH + "rock_boulder_dry_diff_1k.jpg",
+    normalMap: PATH + "rock_boulder_dry_nor_gl_1k.jpg",
+    roughnessMap: PATH + "rock_boulder_dry_rough_1k.jpg",
+    displacementMap: PATH + "rock_boulder_dry_disp_1k.png",
+  });
 
-    // propsTexture.displacementMap.repeat.set(4,64);
-    // propsTexture.displacementMap.wrapS = propsTexture.displacementMap.wrapT = RepeatWrapping;
+  propsTexture.map.repeat.set(4, 64);
+  propsTexture.map.wrapS = propsTexture.map.wrapT = RepeatWrapping;
 
-    // propsTexture.normalMap.repeat.set(4,64);
-    // propsTexture.normalMap.wrapS = propsTexture.normalMap.wrapT = RepeatWrapping;
+  propsTexture.normalMap.repeat.set(4, 64);
+  propsTexture.normalMap.wrapS = propsTexture.normalMap.wrapT = RepeatWrapping;
 
-    // propsTexture.roughnessMap.repeat.set(4,64);
-    // propsTexture.roughnessMap.wrapS = propsTexture.roughnessMap.wrapT = RepeatWrapping;
+  propsTexture.roughnessMap.repeat.set(4, 64);
+  propsTexture.roughnessMap.wrapS = propsTexture.roughnessMap.wrapT = RepeatWrapping;
 
-    return (
-        <group {...props} dispose={null}>
-          <mesh
-            castShadow
-            receiveShadow
-            geometry={nodes.nave_espacial.geometry}
-            material={new MeshBasicMaterial({ color: new Color("red") })}
-            scale={0.25}
-          />
-        </group>
-      )
+  propsTexture.displacementMap.repeat.set(4, 64);
+  propsTexture.displacementMap.wrapS = propsTexture.displacementMap.wrapT = RepeatWrapping;
+
+  return (
+    <group {...props} dispose={null}>
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.nave_espacial.geometry}
+        material={new MeshBasicMaterial({ color: new Color("red") })}
+        scale={0.25}
+        position={[0, 0, 0]}
+      />
+      <mesh name="Floor"
+        geometry={nodesSG.Floor.geometry}
+        material={materialsSG.Material}
+        position={[0, 0, -120]}
+      >
+        <meshStandardMaterial {...propsTexture} />
+      </mesh>
+    </group>
+  )
 }
 
 useGLTF.preload('/assets/models/world/squisgame.glb')
+useGLTF.preload("/assets/models/world/SquidGamesMaik.glb");
