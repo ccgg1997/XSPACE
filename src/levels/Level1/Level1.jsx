d9fdb96190232da373450fb6b929f05acd96758a
Cristian Gómez <89625031+ccgg1997@users.noreply.github.com>
2024-04-27 11:33:24 +0000

    level1, astronauta, movements, background

---
+++ src/levels/Level1/Level1.jsx
@@ -0,0 +1,56 @@
+import { Perf } from "r3f-perf";
+import { KeyboardControls, OrbitControls } from "@react-three/drei";
+import { Physics } from "@react-three/rapier";
+import { Suspense } from "react";
+import WelcomeText from "./abstractions/WelcomeText";
+import RedMen from "./characters/redMen/RedMen";
+import Lights from "./lights/Lights";
+import Environments from "./staging/Environments";
+import { Girl } from "./characters/girl/Girl";
+import { Canvas } from "@react-three/fiber";
+import World from "./world/World";
+import Controls from "./controls/Controls";
+import Avatar from "./characters/avatar/Avatar";
+import useMovements from "../../utils/key-movements-l1";
+import Ecctrl, { EcctrlAnimation } from "ecctrl";
+import { AvatarProvider } from "../../context/AvatarContext";
+
+export default function Level1() {
+    const map = useMovements();
+
+    return (
+        <AvatarProvider>
+            <KeyboardControls map={map} >
+                <Canvas
+                    shadows={false}
+                >
+                    <Perf position="top-left" />
+                    <Suspense fallback={null}>
+                        <Lights ></Lights>
+                        {/* <Environments /> */}
+                        <Physics debug={true}>
+                            <World />
+                            <Girl />
+                            <RedMen />
+                            <Ecctrl
+                                camInitDis={-2}
+                                camMaxDis={-2}
+                                maxVelLimit={5}
+                                jumpVel={4}
+                                position={[1, 10, -10]}
+                                characterInitDir={180}
+                                camInitDir={{ x: 0, y: 10 }}
+                            >
+                                <Avatar />
+                            </Ecctrl>
+                        </Physics>
+
+                        {/* <WelcomeText position={[0, 1, -2]} /> */}
+                    </Suspense>
+                    <Controls />
+                </Canvas>
+            </KeyboardControls>
+        </AvatarProvider>
+
+    )
+}
---
+++ src/levels/Level1/lights/Lights.jsx
@@ -0,0 +1,47 @@
+import { Color } from "three";
+import { useRef, useState, useEffect } from 'react';
+
+const Lights = () => {
+    const pointLightRef = useRef(null);
+    const [lightZ, setLightZ] = useState(0);
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+        setLightZ((prevZ) => (prevZ > -90 ? prevZ - 3 : prevZ)); // Ajusta -200 según tu límite
+        }, 1000); // 1000 milisegundos = 1 segundo
+
+        return () => clearInterval(intervalId);
+    }, []);
+
+    return (
+        <>
+        <ambientLight color={new Color("white")} intensity={0.90} />
+        <directionalLight color={new Color("white")} intensity={0.009} />
+        <pointLight
+            ref={pointLightRef}
+            position={[0, 3, lightZ]}
+            color={new Color("white")}
+            intensity={100}  // Ajusta la intensidad
+            distance={6}    // Ajusta la distancia de influencia
+            decay={1}        // Ajusta la atenuación (opcional)
+        />
+    </>
+    // return <>
+    //     <ambientLight
+    //         color={new Color("white")} 
+    //         intensity={0.05} 
+    //     />
+    //     <directionalLight
+    //         color={new Color("white")} 
+    //         intensity={0.1} 
+    //     />
+    //     <pointLight position={[1, -10, -50]} 
+    //     color={new Color("white")}
+    //     intensity={2000}
+
+
+    //     />
+
+    // </>
+)}
+export default Lights;
